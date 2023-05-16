1fdfae36078c4c5f89d06dca77b52d2d08b07d70
Martin Kratochvíl <mkratochvil2@retia.cz>
2023-05-16 14:53:21 +0200

    created modern head

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../app/globals.css';
+import './FeatureCard.css';
 
 export type Feature = {
 	title: string;
@@ -7,7 +8,7 @@ export type Feature = {
 };
 
 const FeatureCard: React.FC<Feature> = ({title, text}) => (
-	<div className='rounded-3xl p-2 sm:p-4 space-y-2 cursor-pointer hover:translate-y-[-2px] border-solid border-blue-600 border-[1px] bg-transparent shadow-md hover:shadow-lg transition duration-300 ease-in-out'>
+	<div className='rounded-3xl bg-white p-2 sm:p-4 space-y-2 cursor-pointer hover:translate-y-[-2px] bg-transparent shadow-md hover:shadow-lg transition duration-300 ease-in-out'>
 		<h3 className='playfair flex w-full justify-center text-center text-sm md:text-xl lg:text-2xl'>
 			{title}
 		</h3>
