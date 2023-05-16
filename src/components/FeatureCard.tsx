0da17d1ede81b10040952458f827ff3d6dcba2ab
MartinKratochvilProgramy <martvil96@gmail.com>
2023-05-16 21:23:11 +0200

    landing page tweaks

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../app/globals.css';
 import './FeatureCard.css';
+import {useTheme} from 'next-themes';
 
 export type Feature = {
 	title: string;
 	text: string;
 };
 
-const FeatureCard: React.FC<Feature> = ({title, text}) => (
-	<div className='rounded-3xl bg-white p-2 sm:p-4 space-y-2 cursor-pointer hover:translate-y-[-2px] bg-transparent shadow-md hover:shadow-lg transition duration-300 ease-in-out'>
-		<h3 className='playfair flex w-full justify-center text-center text-sm md:text-xl lg:text-2xl'>
-			{title}
-		</h3>
-		<p className='flex w-full justify-center text-center text-gray-400 text-xs md:text-sm lg:text-xl'>
-			{text}
-		</p>
-	</div>
-);
+const FeatureCard: React.FC<Feature> = ({title, text}) => {
+	const [style, setStyle] = useState({background: ''});
+	const {theme} = useTheme();
+
+	useEffect(() => {
+		// Set random location of the gradient circle between min and max %
+		const min = 40;
+		const max = 60;
+		const randomNumberX = Math.floor(Math.random() * (max - min + 1)) + min;
+		const randomNumberY = Math.floor(Math.random() * (max - min + 1)) + min;
+		console.log(theme);
+
+		setStyle({
+			background: `radial-gradient(circle at ${randomNumberX}% ${randomNumberY}%,
+						rgba(30, 144, 231, ${theme === 'light' ? '0.1' : '0.2'}) 0%,
+						rgba(56, 113, 209, ${theme === 'light' ? '0.1' : '0.2'}) 22.9%,
+						rgba(38, 76, 140, ${theme === 'light' ? '0.01' : '0.1'}) 76.7%,
+						rgba(31, 63, 116, ${theme === 'light' ? '0.01' : '0.1'}) 100.2%)`,
+		});
+	}, [theme]);
+
+	return (
+		<div style={style} className='rounded-3xl feature-card bg-white p-2 sm:p-4 space-y-2 md:space-y-4 lg:space-y-6 cursor-pointer hover:translate-y-[-2px] bg-transparent shadow-md hover:shadow-lg transition duration-300 ease-in-out'>
+			<h3 className='playfair flex w-full justify-center text-center text-sm md:text-xl lg:text-2xl'>
+				{title}
+			</h3>
+			<p className='flex w-full justify-center text-center text-gray-600 dark:text-gray-200 opacity-100 dark:opacity-60 text-xs md:text-sm lg:text-lg'>
+				{text}
+			</p>
+		</div>
+	);
+};
 
 export default FeatureCard;
--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -5,7 +5,7 @@ import {type Feature} from './FeatureCard';
 import '../app/globals.css';
 
 const Features = () => {
-	const features: Feature[] = [
+	const featuresLong: Feature[] = [
 		{
 			title: 'Streamlined Investment Management',
 			text: 'Say goodbye to scattered spreadsheets and complex systems - with Daily Portfolio Management, you can view your holdings, track performance, and make informed decisions all from the palm of your hand.',
@@ -23,16 +23,34 @@ const Features = () => {
 			text: 'Your financial information is highly sensitive, and we understand the importance of security. We prioritize data protection and employ advanced encryption measures to safeguard your personal and financial data, giving you peace of mind as you manage your investments.',
 		},
 	];
+	const featuresShort: Feature[] = [
+		{
+			title: 'Investment Management',
+			text: 'Say goodbye to scattered spreadsheets and complex systems - with Daily Portfolio Management, you can view your holdings, track performance, and make informed decisions all from the palm of your hand.',
+		},
+		{
+			title: 'Real-Time Stock Updates',
+			text: 'Stay informed and seize opportunities with real-time stock updates. Access the latest market data and track trends, so that you never miss a crucial investment opportunity.',
+		},
+		{
+			title: 'Seamless Stock Addition',
+			text: 'Adding stocks to your portfolio has never been easier. Daily Portfolio Management simplifies the process, enabling you to search, select, and add stocks with just a few clicks.',
+		},
+		{
+			title: 'Secure and Reliable Data',
+			text: 'Your financial information is highly sensitive, and we understand the importance of security. We prioritize data protection and employ advanced encryption measures.',
+		},
+	];
 
 	return (
 
 		<Container className='flex flex-wrap justify-center'>
-			<div className='flex flex-col items-center w-full space-x-4 lg:space-y-12'>
-				<h1 className='playfair flex justify-center w-full font-inter text-center text-2xl lg:text-2xl xl:text-4xl font-bold leading-snug tracking-tight text-gray-700 lg:leading-tight xl:leading-tight dark:text-gray-200'>
+			<div className='flex flex-col items-center w-full space-y-8 lg:space-y-12'>
+				<h1 className='playfair flex justify-center w-full font-inter text-center text-2xl lg:text-4xl font-bold leading-snug tracking-tight text-gray-700 lg:leading-tight xl:leading-tight dark:text-gray-200'>
                     Everything you need to keep an eye on your finances
 				</h1>
-				<div className='grid grid-cols-2 xl:grid-cols-4 gap-4' id='feature-cards'>
-					{features.map((feature: Feature) => (
+				<div className='grid grid-cols-2 xl:grid-cols-4 gap-4 md:gap-6 lg:gap-8' id='feature-cards'>
+					{featuresShort.map((feature: Feature) => (
 						<FeatureCard title={feature.title} text={feature.text} key={feature.title} />
 					))}
 				</div>
