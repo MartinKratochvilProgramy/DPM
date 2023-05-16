080a9cf721f718c1452f3aaa01a0aa6b25372259
Martin Kratochvíl <mkratochvil2@retia.cz>
2023-05-16 14:35:05 +0200

    working on demo

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -7,11 +7,11 @@ export type Feature = {
 };
 
 const FeatureCard: React.FC<Feature> = ({title, text}) => (
-	<div className='rounded-xl p-2 sm:p-4 space-y-2 cursor-pointer hover:translate-y-[-2px] border-solid border-blue-600 border-[1px] bg-transparent dark:hover:border-none dark:hover:text-gray-800 dark:hover:bg-gray-200 shadow-md hover:shadow-lg transition duration-300 ease-in-out'>
+	<div className='rounded-3xl p-2 sm:p-4 space-y-2 cursor-pointer hover:translate-y-[-2px] border-solid border-blue-600 border-[1px] bg-transparent shadow-md hover:shadow-lg transition duration-300 ease-in-out'>
 		<h3 className='playfair flex w-full justify-center text-center text-sm md:text-xl lg:text-2xl'>
 			{title}
 		</h3>
-		<p className='flex w-full justify-center text-center text-gray-500 text-xs md:text-sm lg:text-xl'>
+		<p className='flex w-full justify-center text-center text-gray-400 text-xs md:text-sm lg:text-xl'>
 			{text}
 		</p>
 	</div>
