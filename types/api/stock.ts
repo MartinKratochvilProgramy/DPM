93f57b91226fc0c948117a727f578db64f5d2b1d
MartinKratochvilProgramy <martvil96@gmail.com>
2023-05-17 10:46:47 +0200

    added types

---
+++ types/api/stock.ts
@@ -0,0 +1,48 @@
+interface PurchaseInterface {
+  date: string
+  amount: number
+  currentPrice: number
+  totalAmount: number
+}
+
+export interface FormattedStockInterface {
+  ticker: string
+  amount: number
+  prevClose: number
+  _id: string
+  firstPurchase: string
+  lastPurchase: string
+  purchaseHistory: PurchaseInterface[]
+}
+
+export interface StockInterface {
+  ticker: string
+  amount: number
+  prevClose: number
+}
+
+export interface PurchaseHistoryInterface {
+  ticker: string
+  purchases: PurchaseInterface[]
+}
+
+export interface TotalInvestedHistoryInterface {
+  date: string
+  total: number
+}
+
+export interface StocksInterface {
+  username: string
+  stocks: StockInterface[]
+  purchaseHistory: PurchaseHistoryInterface[]
+  netWorthHistory: Array<{
+    date: string
+    netWorth: number
+  }>
+  relativeChangeHistory: Array<{
+    date: string
+    relativeChange: number
+  }>
+  totalInvestedHistory: TotalInvestedHistoryInterface[]
+
+}
