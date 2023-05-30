11ba2c9d1c448fac9799aaf369132d9f6fa03fdb
MartinKratochvilProgramy <martvil96@gmail.com>
2023-05-30 15:57:15 +0200

    Make stock layout respnsive

--- src/components/MainPage/Stocks/Stock.tsx
+++ src/components/MainPage/Stocks/Stock.tsx
@@ -88,15 +88,15 @@ export const Stock: React.FC<Props> = ({
   return (
     <>
       <div
-        className="bg-white dark:bg-transparent border-solid border-[1px] border-blue-400 dark:border-gray-500 rounded-xl px-3 md:px-4 py-3 my-2 text-black dark:text-gray-400 font-medium text-xs sm:text-sm leading-snug cursor-pointer uppercase hover:shadow-lg transition duration-150 ease-in-out"
+        className="bg-white dark:bg-transparent border-solid border-[1px] border-blue-400 dark:border-gray-500 rounded-xl px-2 md:px-5 py-2 md:py-3 my-2 text-black dark:text-gray-400 font-medium text-xs sm:text-sm leading-snug cursor-pointer uppercase hover:shadow-lg transition duration-150 ease-in-out"
         onClick={() => { expand() }}
       >
-        <div className="flex flex-row items-center pl-2 pr-1 mb-1">
-          <div className="w-full dark:text-gray-100 h-full flex items-start">
-            <div className="w-[52px] xsm:w-24 md:w-[81px] font-bold text-justify">{stock.ticker}</div>
-            <div className="w-14 xsm:w-[76px] md:w-24 flex justify-center">{stock.amount}</div>
-            <div className="w-14 xsm:w-[76px] md:w-24 flex justify-center">{stock.prevClose.toFixed(2)}</div>
-            <div className="w-14 xsm:w-[76px] md:w-24 flex justify-center">
+        <div className="flex flex-row w-full items-center justify-between mb-1">
+          <div className='flex flex-row justify-start sm:justify-between w-full max-w-[400px] px-1 dark:text-gray-100'>
+            <div className="w-[50px] md:w-[70px] flex justify-start">{stock.ticker}</div>
+            <div className="w-[50px] md:w-[70px] flex justify-center">{stock.amount}</div>
+            <div className="w-[50px] md:w-[70px] flex justify-center">{stock.prevClose.toFixed(2)}</div>
+            <div className="w-[50px] md:w-[70px] flex justify-center">
               {stock.avgPercentageChange >= 0
                 ? <div className="text-green-600">{`+${stock.avgPercentageChange}%`}</div>
                 : <div className="text-red-600">{`${stock.avgPercentageChange}%`}</div>
@@ -114,50 +114,47 @@ export const Stock: React.FC<Props> = ({
           </div>
         </div>
 
-        <div>
-          {expanded &&
+        {expanded &&
             <div className="flex flex-col items-start justify-start border-t-[1px] border-t-gray-500 space-y-2">
-              <div className="flex flex-row mt-4 dark:text-gray-100 px-2">
-                <div className="w-[60px] xsm:w-24 md:w-[81px] font-bold text-justify">DATE</div>
-                <div className="w-14 xsm:w-[76px] md:w-24 flex justify-center font-bold">AMOUNT</div>
-                <div className="w-14 xsm:w-[76px] md:w-24 flex justify-center font-bold">PRICE</div>
-                <div className="w-14 xsm:w-[76px] md:w-24 flex justify-center font-bold">CHANGE</div>
+              <div className="flex flex-row justify-start sm:justify-between w-full max-w-[400px] mt-4 px-1 dark:text-gray-100">
+                <div className="w-[50px] sm:w-[70px] flex justify-start font-bold">DATE</div>
+                <div className="w-[50px] sm:w-[70px] flex justify-center font-bold">AMOUNT</div>
+                <div className="w-[50px] sm:w-[70px] flex justify-center font-bold">PRICE</div>
+                <div className="w-[50px] sm:w-[70px] flex justify-center font-bold">CHANGE</div>
               </div>
 
-              <div className='flex flex-col w-full'>
-                {stock.purchases.map((purchase: PurchaseInterface, i) => {
-                  let [day, month, year] = formatDate(purchase.date).split('/')
-                  if (day.length === 1) day = '0' + day
-                  if (month.length === 1) month = '0' + month
-                  year = year.substring(2, 4)
+              {stock.purchases.map((purchase: PurchaseInterface, i) => {
+                let [day, month, year] = formatDate(purchase.date).split('/')
+                if (day.length === 1) day = '0' + day
+                if (month.length === 1) month = '0' + month
+                year = year.substring(2, 4)
 
-                  return (
-                    <div key={purchase.id} className="flex flex-row items-center rounded-xl pl-2 pr-1 py-1 hover:bg-gray-100 dark:hover:bg-opacity-5 mr-1">
-                      <div className="w-full h-full flex items-start">
-                        <div className="w-[60px] xsm:w-24 md:w-[81px] text-justify">{day}-{month}-{year}</div>
-                        <div className="w-14 xsm:w-[76px] md:w-24 flex justify-center">{purchase.amount}</div>
-                        <div className="w-14 xsm:w-[76px] md:w-24 flex justify-center">{purchase.price}</div>
-                        <div className="w-14 xsm:w-[76px] md:w-24 flex justify-center">
-                          {purchase.relativeChange >= 0
-                            ? <div className="text-green-600">{'+' + purchase.relativeChange.toFixed(1) + '%'}</div>
-                            : <div className="text-red-600">{purchase.relativeChange.toFixed(1) + '%'}</div>
-                          }
-                        </div>
-                      </div>
-                      <div
-                        onClick={(e) => { openDeleteStockModal(e, purchase.amount, purchase.id) }}
-                        id={stock.ticker}
-                        className="rounded-full flex justify-center items-center w-6 h-6 transition duration-150 hover:bg-red-100 dark:hover:bg-red-500 dark:hover:bg-opacity-50 ease-in-out"
-                      >
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1" stroke={theme === 'light' ? 'black' : 'white'} className="w-4 h-4">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                        </svg>
+                return (
+                  <div key={purchase.id} className="flex flex-row w-full justify-start sm:justify-between items-center rounded-xl px-1 py-1 hover:bg-gray-100 dark:hover:bg-opacity-5 mr-1">
+                    <div className="flex flex-row w-full max-w-[400px] px-1 justify-start sm:justify-between rounded-xl hover:bg-gray-100 dark:hover:bg-opacity-5 h-full">
+                      <div className="w-[50px] sm:w-[70px] flex items-center justify-start">{day}-{month}-{year}</div>
+                      <div className="w-[50px] sm:w-[70px] flex items-center justify-center">{purchase.amount}</div>
+                      <div className="w-[50px] sm:w-[70px] flex items-center justify-center">{purchase.price}</div>
+                      <div className="w-[50px] sm:w-[70px] flex items-center justify-center">
+                        {purchase.relativeChange >= 0
+                          ? <div className="text-green-600">{'+' + purchase.relativeChange.toFixed(1) + '%'}</div>
+                          : <div className="text-red-600">{purchase.relativeChange.toFixed(1) + '%'}</div>
+                        }
                       </div>
                     </div>
+                    <div
+                      onClick={(e) => { openDeleteStockModal(e, purchase.amount, purchase.id) }}
+                      id={stock.ticker}
+                      className="rounded-full flex justify-center items-center w-6 h-6 transition duration-150 hover:bg-red-100 dark:hover:bg-red-500 dark:hover:bg-opacity-50 ease-in-out"
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1" stroke={theme === 'light' ? 'black' : 'white'} className="w-4 h-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                      </svg>
+                    </div>
+                  </div>
 
-                  )
-                })}
-              </div>
+                )
+              })}
 
               <div className="flex w-full justify-center items-center gap-2">
                 <OrderDropDown values={['6m', '1y', '2y', '5y']} orderDropdownValue={period} setOrderDropdownValue={setPeriod} handleClick={handleDropdownClick} theme={'light'} />
@@ -173,9 +170,8 @@ export const Stock: React.FC<Props> = ({
                 <StockChartModal stockTicker={stock.ticker} stockHistory={stockHistory} purchaseHistory={stock.purchases} loadingData={loadingData} dataLoaded={dataLoaded} stockChartLoadingError={stockChartLoadingError} />
               }
             </div>
-          }
+        }
 
-        </div>
       </div>
       <DeleteStockModal
         showDeleteModal={showDeleteModal}
