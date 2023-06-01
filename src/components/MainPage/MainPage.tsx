4b649636f434078e1f4c7f7ec79d648c0608aa63
MartinKratochvilProgramy <martvil96@gmail.com>
2023-06-01 21:45:34 +0200

    Set max-w on modal

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -95,7 +95,7 @@ const MainPage = () => {
         aria-describedby="show-detailed-stock-chart"
       >
         <div>
-          <div className='fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 overflow-y-auto bg-gray-100 dark:bg-[#1e2836] opacity-[0.96] rounded-xl  aspect-[1.2] h-[80vh] border-solid border-[1px] border-blue-400 dark:border-gray-500'>
+          <div className='fixed max-w-[600px] left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 overflow-y-auto bg-gray-100 dark:bg-[#1e2836] opacity-[0.96] rounded-xl  aspect-[1.2] h-[80vh] border-solid border-[1px] border-blue-400 dark:border-gray-500'>
             <Stocks
               stocks={stocks}
               orderDropdownValue={orderDropdownValue}
