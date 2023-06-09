e91776a537dfd7f0eed7a136824283327f0dadd6
MartinKratochvilProgramy <martvil96@gmail.com>
2023-06-09 13:31:24 +0200

    Disable backProp on input elements

--- src/components/MainPage/Stocks.tsx
+++ src/components/MainPage/Stocks.tsx
@@ -93,6 +93,7 @@ export const Stocks: React.FC<Props> = ({
         />
         <input
           onChange={(e) => { setSearchKey(e.target.value) }}
+          onClick={(e) => { e.stopPropagation() }}
           className='w-[105px] shadow-sm px-3 py-0 sm:py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-md transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
           type="text"
           placeholder='Search...' />
