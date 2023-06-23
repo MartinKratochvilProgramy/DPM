38ea520482bdee1402b6ec5118c9d7d41e6e2e55
Martin Kratochvíl <mkratochvil2@retia.cz>
2023-06-23 09:59:28 +0200

    Add update accounts

---
+++ pages/api/update_accounts.ts
@@ -0,0 +1,24 @@
+import { type NextApiRequest, type NextApiResponse } from 'next'
+import { updateAccount } from '@/utils/api/updateAccount'
+import prisma from '@/lib/prisma'
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  // remove stock from db
+
+  try {
+    const users = await prisma.user.findMany()
+
+    const responses = []
+
+    for (let i = 0; i < users.length; i++) {
+      const user = users[i]
+      const response = await updateAccount(user.email)
+      responses.push(response)
+    }
+
+    res.status(200).json(responses)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+};
