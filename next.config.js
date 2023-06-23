65ae51f7ef28e303999a5b5142876be89f0ad3e6
Martin Kratochvíl <mkratochvil2@retia.cz>
2023-06-23 08:58:39 +0200

    Set signup route dependent on .env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL
+    // Add more environment variables as needed
+  }
+}
 
 module.exports = nextConfig
