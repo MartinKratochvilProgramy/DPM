36299e1bf8bf9410f33ae7a4d161f07d6232a30b
Martin Kratochvíl <mkratochvil2@retia.cz>
2023-10-26 06:47:48 +0200

    Add copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import Container from './Container'
 
 const Footer = () => {
   return (
-    <footer className='bg-blue-400 flex w-full h-full justify-center items-center'>
-      <Container className='flex flex-wrap items-between pb-0 pt-0'>
-        <div className='flex w-full h-full justify-center items-center'>
-            Footer
-        </div>
-      </Container>
+    <footer className='bg-blue-transparent flex w-full justify-center items-start'>
+      <div className='flex w-full justify-center items-start text-sm text-gray-500'>
+        &#169; Martin Kratochvíl
+      </div>
     </footer>
   )
 }
